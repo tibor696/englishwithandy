7fd6f5432a5de2f66b475eacc71460dd915e666d
tibor696 <siladi.tibor@gmail.com>
2026-04-22 22:01:56 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,13 +2,13 @@
 return [
     'smtp_host' => "smtp.gmail.com",
     'smtp_port' => 465,
-    'smtp_user' => "kavecansky@gmail.com",
-    'smtp_pass' => "",
+    'smtp_user' => "tibortestovaci5@gmail.com",
+    'smtp_pass' => "dlyu wmmf zzhr mkfz",
 
-    'sender_email' => "kavecansky@gmail.com", // email a meno odosielatela
-    'sender_name' => "Kontaktný formulár",
+    'sender_email' => "tibortestovaci5@gmail.com", // email a meno odosielatela
+    'sender_name' => "English with Andy",
 
-    'to_email' => "kavecansky@gmail.com", // email adresata
+    'to_email' => "englishwithandyba@gmail.com", // email adresata
     
     'subject' => "Kontaktný formulár", // predmet mailu
 ];
